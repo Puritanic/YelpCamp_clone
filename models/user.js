78463ac6cb33b32b0c8ea87535260baaaf0f5136
Darkø Tasevski <darktasevski@gmail.com>
2017-07-01 14:08:47 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -8,20 +8,15 @@ passportLocalMongoose   = require('passport-local-mongoose');
 var UserSchema = new mongoose.Schema({
 <<<<<<< HEAD
     username: {type: String, unique: true, required: true},
-=======
     username: String,
->>>>>>> 87a9c339e1faea75494a45ac0c60412f6aca3b61
     password: String,
     avatar: String,
     firstName: String,
     lastName: String,
-<<<<<<< HEAD
     email: {type: String, unique: true, required: true},
     resetPasswordToken: String,
     resetPasswordExpires: Date,
-=======
     email: String,
->>>>>>> 87a9c339e1faea75494a45ac0c60412f6aca3b61
     isAdmin: {
         type: Boolean,
         default: false
